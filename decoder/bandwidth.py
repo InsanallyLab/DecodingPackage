9b35158ffc8bdfd264a5ddb32f043051694d068f
Pr1yansh1 <priyanshigarg068@gmail.com>
2023-06-27 14:39:47 -0400

    bandwidth.py sessionfile dependencies

--- decoder/bandwidth.py
+++ decoder/bandwidth.py
@@ -9,13 +9,13 @@ def getBWs_elife2019():
     #RNN
     return np.linspace(.005, 0.305, 11)
 
-def getLogISIs(sessionfile,clust,trials,interval): #This code is probably redundant. Could be replaced entirely by cacheLogISIs
+def getLogISIs(loader,clust,trials,interval): #This code is probably redundant. Could be replaced entirely by cacheLogISIs
     ISIs = []
     times = []
     for trial in trials:
-        starttime,endtime = interval._ToTimestamp(sessionfile,trial)
-        spiketimes = util.getSpikeTimes(sessionfile,clust=clust,starttime = starttime, endtime = endtime)
-        spiketimes = spiketimes * 1000 / sessionfile.meta.fs
+        starttime,endtime = interval._ToTimestamp(loader.trials,trial)
+        spiketimes = util.getSpikeTimes(loader.spikes,clust=clust,starttime = starttime, endtime = endtime)
+        spiketimes = spiketimes * 1000 / loader.meta.fs
 
         ISIs.append(np.diff(spiketimes))
         times.append(spiketimes[1:])
@@ -25,14 +25,14 @@ def getLogISIs(sessionfile,clust,trials,interval): #This code is probably redund
     times = np.concatenate(times)
     return LogISIs, times
   
-def sklearn_grid_search_bw(sessionfile,clust,trialsPerDayLoaded,interval,folds = 50):
-    conditions = util.getAllConditions(sessionfile,clust,trialsPerDayLoaded=trialsPerDayLoaded)
+def sklearn_grid_search_bw(loader,clust,trialsPerDayLoaded,interval,folds = 50):
+    conditions = util.getAllConditions(loader,clust,trialsPerDayLoaded=trialsPerDayLoaded)
     
     trialsToUse = conditions['all_trials'].trials
     trialsToUse = np.random.permutation(trialsToUse)
     trialsToUse = trialsToUse[0:int(len(trialsToUse)/2)]
 
-    LogISIs,_ = getLogISIs(sessionfile,clust,trialsToUse,interval)
+    LogISIs,_ = getLogISIs(loader,clust,trialsToUse,interval)
     #Ensure that we don't try to cross validate more than there are ISIs
     folds = np.min([folds,len(LogISIs)])
 
