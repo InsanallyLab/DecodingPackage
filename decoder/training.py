8ec23dde575fa0ed676cdf969bc59dfe23d3524b
Pr1yansh1 <priyanshigarg068@gmail.com>
2023-06-27 14:35:09 -0400

    training.py sessionfile dependencies

--- decoder/training.py
+++ decoder/training.py
@@ -36,7 +36,7 @@ def synthetic_spiketrain(model,trial_length=2500):#only model.all is required to
             break
     return np.array(LogISIs)
 
-def cachedpredictTrial(sessionfile,clust,model,trialISIs,conditions = ['target_tone','nontarget_tone'],synthetic = False):
+def cachedpredictTrial(model,trialISIs,conditions = ['target_tone','nontarget_tone'],synthetic = False):
     if synthetic:
         LogISIs = synthetic_spiketrain(model)
     else:
@@ -53,11 +53,6 @@ def cachedpredictTrial(sessionfile,clust,model,trialISIs,conditions = ['target_t
     for cond in conditions:
         probabilities[cond].prob = np.cumsum(np.log10(np.concatenate((   [model.conds[cond].Prior_0] , model.conds[cond].Likelihood(LogISIs)   ))))
 
-    ##Calculate change in W. LLR after each LogISI
-    #for idx,logISI in enumerate(LogISIs):
-    #    for cond in conditions:
-    #        probabilities[cond].prob[idx+1] = probabilities[cond].prob[idx] + np.log10(model[cond].Likelihood.evaluate(logISI))
-
     #Exponentiate back from log so that normalization can take place
     for cond in conditions:
         probabilities[cond].prob = np.power(10,probabilities[cond].prob)
@@ -71,7 +66,6 @@ def cachedpredictTrial(sessionfile,clust,model,trialISIs,conditions = ['target_t
     for cond in conditions:
         probabilities[cond].prob /= sum_of_probs
 
-
     #No ISIs in trial. guess instead.
     if len(LogISIs) < 1:
         keys = [cond for cond in probabilities]
@@ -81,7 +75,6 @@ def cachedpredictTrial(sessionfile,clust,model,trialISIs,conditions = ['target_t
             maxidx = np.random.permutation(maxidx)
         maxidx = maxidx[0]
         maxCond = keys[maxidx]
-        #return None,None,None,None
         return maxCond,probs[maxidx],probabilities,True
     #ISIs were present in the trial. We can take out a proper choice by the algorithm.
     else:
@@ -91,8 +84,7 @@ def cachedpredictTrial(sessionfile,clust,model,trialISIs,conditions = ['target_t
         maxidx = np.argmax(probs)
         maxCond = keys[maxidx]
         return maxCond,probs[maxidx], probabilities,False
-        #return maxCond,probabilities[maxCond].prob[len(times)-1], probabilities,times
-
+      
 def cachedtrainDecodingAlgorithm(loader,clust,trialsPerDayLoaded,bw,cachedLogISIs,Train_X,condition_names,synthetic = False):
     model = SimpleNamespace()
     model.conds = dict()
@@ -142,7 +134,7 @@ def cachedtrainDecodingAlgorithm(loader,clust,trialsPerDayLoaded,bw,cachedLogISI
             print(f"Skipping fold. Not enough ISIs for the {cond} condition")
             return None
 
-        #LogISIs,_ = getLogISIs(sessionfile,clust,decoding_conditions[cond].trials)
+        #LogISIs,_ = getLogISIs(loader,clust,decoding_conditions[cond].trials)
 
         f,inv_f = LogISIsToLikelihoods(LogISIs,bw) #This is a gaussian KDE
         model.conds[cond].Likelihood = f
@@ -157,9 +149,9 @@ def cachedtrainDecodingAlgorithm(loader,clust,trialsPerDayLoaded,bw,cachedLogISI
         
     return model
 
-def cachedcalculateAccuracyOnFold(sessionfile,clust,model,cachedLogISIs,Test_X,weights,conditions=['target','nontarget'],synthetic=False):
+def cachedcalculateAccuracyOnFold(loader,clust,model,cachedLogISIs,Test_X,weights,conditions=['target','nontarget'],synthetic=False):
     #Note: this call to getAllConditions uses NO_TRIM all the time because it is only used to determine correctness
-    all_conditions = getAllConditions(sessionfile,clust,trialsPerDayLoaded='NO_TRIM')
+    all_conditions = getAllConditions(loader,clust,trialsPerDayLoaded='NO_TRIM')
     
     accumulated_correct = 0
     #accumulated_total = 0 # I have changed how weighting works to be weighted by condition prevalence so this is no longer necessary
@@ -168,7 +160,7 @@ def cachedcalculateAccuracyOnFold(sessionfile,clust,model,cachedLogISIs,Test_X,w
     num_empty = 0
     
     for trial in Test_X:
-        cond,_,_,empty_ISIs = cachedpredictTrial(sessionfile,clust,model,cachedLogISIs[trial],conditions=conditions,synthetic=synthetic)
+        cond,_,_,empty_ISIs = cachedpredictTrial(loader,clust,model,cachedLogISIs[trial],conditions=conditions,synthetic=synthetic)
         
         if cond is None:
             continue
