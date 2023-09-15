89354895222e8ac9694db4c69ca1da5b653a0155
Caleb Jackson <jacksoncaleb.calebjackson@gmail.com>
2023-09-15 15:47:27 -0400

    Add EventSet tests for mismatched timestamps and labels

--- tests/event_set_test.py
+++ tests/event_set_test.py
@@ -9,3 +9,21 @@ def test_eventSet_initialization():
     labels = ["ex1", "ex2", "ex3"]
     event_set = EventSet(event_name, timestamps, labels)
     assert len(event_set.events) == 3
+
+
+def test_eventSet_timestamps_shorter_than_labels():
+    """Test if EventSet raises a ValueError when there are less timestamps than labels"""
+    event_name = "test"
+    timestamps = [5, 10]
+    labels = ["ex1", "ex2", "ex3"]
+    with pytest.raises(ValueError):
+        event_set = EventSet(event_name, timestamps, labels)
+
+
+def test_eventSet_labels_shorter_than_timestamps():
+    """Test if EventSet raises a ValueError when there are less labels than timestamps"""
+    event_name = "test"
+    timestamps = [5, 10, 15]
+    labels = ["ex1", "ex2"]
+    with pytest.raises(ValueError):
+        event_set = EventSet(event_name, timestamps, labels)
