75fe1442a4b185d26d4ae130603d5de86c917134
Caleb Jackson <jacksoncaleb.calebjackson@gmail.com>
2023-09-17 16:53:21 -0400

    Add EventSet test for adding valid events

--- tests/event_set_test.py
+++ tests/event_set_test.py
@@ -38,6 +38,7 @@ def test_eventSet_invalid_label():
     with pytest.raises(ValueError):
         event_set = EventSet(event_name, timestamps, labels, possible_values)
 
+
 # TODO: Test that a ValueError is raised when an event with an existing timestamp is added
 # def test_add_event_with_existing_timestamp():
 #     event_name = "test"
@@ -49,3 +50,20 @@ def test_eventSet_invalid_label():
 
 #     with pytest.raises(ValueError):
 #         event_set.add_event(existing_timestamp, new_label)
+
+
+def test_add_valid_event():
+    """Test if EventSet adds valid events"""
+    event_name = "test"
+    timestamps = [5, 10, 15]
+    labels = ["ex1", "ex2", "ex3"]
+    event_set = EventSet(event_name, timestamps, labels,
+                         possible_values=["ex1", "ex2", "ex3", "ex4"])
+    new_timestamp = 20
+    new_label = "ex4"
+
+    event_set.add_event(new_timestamp, new_label)
+
+    assert event_set.events[new_timestamp] == new_label
+    assert event_set.get_sorted_events() == sorted(
+        timestamps + [new_timestamp])
